ffcefd7579706f5876d4043a28453fbfefd4314d
KeithDeb <keithdebattista@gmail.com>
2022-02-01 21:22:55 +0100

    Basic TaskSystem

---
+++ Assets/Scripts/TaskSystem.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TaskSystem
+{
+    public class TaskSystem
+    {
+        public class Task
+        {
+
+        }
+
+        //List of available Tasks to do
+        private List<Task> taskList;
+
+        //constructor
+        public TaskSystem()
+        {
+            taskList = new List<Task>();
+        }
+
+        public Task RequestNextTask()
+        {
+            //Worker requesting a task
+            if (taskList.Count > 0)
+            {
+                //task reference/give worker first task
+                Task task = taskList[0];
+                taskList.RemoveAt(0);
+
+                return task;
+            }
+            else
+            {
+                //no tasks avaialble
+                return null;
+            }
+        }
+
+        public void AddTask(Task task)
+        {
+            //add a task to the list
+            taskList.Add(task);
+        }
+
+    }
+}
