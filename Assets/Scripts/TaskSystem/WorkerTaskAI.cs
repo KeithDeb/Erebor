4b7f9412c7c6643d67c1d5a090803ffac0fcfef6
KeithDeb <keithdebattista@gmail.com>
2022-02-01 21:54:37 +0100

    Task Timer

--- Assets/Scripts/TaskSystem/WorkerTaskAI.cs
+++ Assets/Scripts/TaskSystem/WorkerTaskAI.cs
@@ -14,9 +14,35 @@ namespace TaskSystem
         }
         private WorkerTaskAI worker;
         private State state;
+        private float waitingTimer;
+
         public void Setup(IWorker worker)
         {
             this.worker = worker;
+            //initial state
+            state = State.WaitingForNextTask;
+        }
+
+        private void Update()
+        {
+            switch (state)
+            {
+                case State.WaitingForNextTask:
+                    //waiting to request new task
+                    waitingTimer -= Time.deltaTime;
+                    if (waitingTimer <= 0)
+                    {
+                        //reset timer
+                        float waitingTimerMax = .2f; //200ms
+                        waitingTimer = waitingTimerMax;
+                        RequestNextTask();
+                    }
+                    break;
+            }
+        }
+        private void RequestNextTask()
+        {
+            Debug.Log("Request Next Task!!");
         }
     }
 }
